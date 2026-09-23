fd7c643fa540af8b355c4f010b2d1f9d9dee8d18
Bimo Prakoso Batmomolin <bimo.batmomolin.1@gmail.com>
2026-09-23 17:56:22 +0800

    Initial commit Modul3_RPL

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ angka2 = float(input("Masukan angka kedua: "))
 hasil = angka1 + angka2 
 
 print(f"Status: Setup Berhasil")
-print(f"Hasil penjumlahan: {hasil}")
+print(f"Hasil Penjumlahan: {hasil}")
 
