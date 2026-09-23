230c85acf2ff9c86ae1fd1f226ad0965910dc73c
Bimo Prakoso Batmomolin <bimo.batmomolin.1@gmail.com>
2026-09-23 16:09:33 +0800

    Initial commit Modul3_RPL

---
+++ main.py
@@ -0,0 +1,9 @@
+print("=== Kalkulator Terminal Modul 3 ===")
+angka1 = float(input("Masukan angka pertama: "))
+angka2 = float(input("Masukan angka kedua: "))
+
+hasil = angka1 + angka2 
+
+print(f"Status: Setup Berhasil")
+print(f"Hasil penjumlahan: {hasil}")
+
